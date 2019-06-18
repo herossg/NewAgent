9a491538343c2df24d2fb3619629088f7f231a20
User <User@User-PC>
2019-06-18 17:34:04 +0900

    회사에서

--- Agent/src/main/java/com/agent/ClientMessage.java
+++ Agent/src/main/java/com/agent/ClientMessage.java
@@ -1,32 +1,27 @@
 package com.agent;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.Serializable;
-
-import javax.imageio.ImageIO;
 
 public class ClientMessage implements Serializable {
     private static final long serialVersionUID = 1L;
     private String mUserid;
     private int mMsgid;
-    private String mAd_flag;
     private String mBtn1;
     private String mBtn2;
     private String mBtn3;
     private String mBtn4;
     private String mBtn5;
-    private String mImg_link;
-    private String mImg_url;
+    private String m1stMessage_type;
+    private String m2ndMessage_type;
     private String mMessage;
     private String mProfile;
     private String mTmpl_id;
-    private String m1stMessage_type;
-    private String m2ndMessage_type;
-    private String mMsg_title;
     private String mSender;
     private String mReserve_date;
+    private String mReg_dt;
     private String mMMS1;
     private String mMMS2;
     private String mMMS3;
@@ -35,9 +30,19 @@ public class ClientMessage implements Serializable {
     private byte[] mImg2;
     private byte[] mImg3;
     private byte[] mImg4;
+    private String mAdd1;
+    private String mAdd2;
+    private String mAdd3;
+    private String mAdd4;
+    private String mAdd5;
     
-    private String[] mPhoneList;
-    private int[] mDeatilMsgid;
+    private StringBuffer mPhnList;
+    
+    /* 수신결과 관련 */
+    private String mPhn;
+    private String mResultCode;
+    private String mResultDate;
+	private String mResultMsg;
     
 	public String getUserid() {
 		return mUserid;
@@ -53,14 +58,6 @@ public class ClientMessage implements Serializable {
 
 	public void setMsgid(int mMsgid) {
 		this.mMsgid = mMsgid;
-	}
-
-	public String getAd_flag() {
-		return mAd_flag;
-	}
-
-	public void setAd_flag(String mAd_flag) {
-		this.mAd_flag = mAd_flag;
 	}
 
 	public String getBtn1() {
@@ -103,22 +100,6 @@ public class ClientMessage implements Serializable {
 		this.mBtn5 = mBtn5;
 	}
 
-	public String getImg_link() {
-		return mImg_link;
-	}
-
-	public void setImg_link(String mImg_link) {
-		this.mImg_link = mImg_link;
-	}
-
-	public String getImg_url() {
-		return mImg_url;
-	}
-
-	public void setImg_url(String mImg_url) {
-		this.mImg_url = mImg_url;
-	}
-
 	public String getMessage() {
 		return mMessage;
 	}
@@ -159,14 +140,6 @@ public class ClientMessage implements Serializable {
 		this.m2ndMessage_type = m2ndMessage_type;
 	}
 
-	public String getsg_title() {
-		return mMsg_title;
-	}
-
-	public void setMsg_title(String mMsg_title) {
-		this.mMsg_title = mMsg_title;
-	}
-
 	public String getSender() {
 		return mSender;
 	}
@@ -189,6 +162,17 @@ public class ClientMessage implements Serializable {
 
 	public void setMMS1(String mMMS1) {
 		this.mMMS1 = mMMS1;
+		if(!this.mMMS1.isEmpty()) {
+			File imgfile = new File(mMMS1);
+			this.mImg1 = new byte[(int) imgfile.length()];
+			try {
+				DataInputStream imgis = new DataInputStream(new FileInputStream(imgfile));
+				imgis.readFully(this.mImg1);
+				imgis.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public String getMMS2() {
@@ -197,6 +181,17 @@ public class ClientMessage implements Serializable {
 
 	public void setMMS2(String mMMS2) {
 		this.mMMS2 = mMMS2;
+		if(!this.mMMS2.isEmpty()) {
+			File imgfile = new File(mMMS2);
+			this.mImg2 = new byte[(int) imgfile.length()];
+			try {
+				DataInputStream imgis = new DataInputStream(new FileInputStream(imgfile));
+				imgis.readFully(this.mImg2);
+				imgis.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public String getMMS3() {
@@ -205,6 +200,17 @@ public class ClientMessage implements Serializable {
 
 	public void setMMS3(String mMMS3) {
 		this.mMMS3 = mMMS3;
+		if(!this.mMMS3.isEmpty()) {
+			File imgfile = new File(mMMS3);
+			this.mImg3 = new byte[(int) imgfile.length()];
+			try {
+				DataInputStream imgis = new DataInputStream(new FileInputStream(imgfile));
+				imgis.readFully(this.mImg3);
+				imgis.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public String getMMS4() {
@@ -213,6 +219,17 @@ public class ClientMessage implements Serializable {
 
 	public void setMMS4(String mMMS4) {
 		this.mMMS4 = mMMS4;
+		if(!this.mMMS4.isEmpty()) {
+			File imgfile = new File(mMMS4);
+			this.mImg4 = new byte[(int) imgfile.length()];
+			try {
+				DataInputStream imgis = new DataInputStream(new FileInputStream(imgfile));
+				imgis.readFully(this.mImg4);
+				imgis.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public byte[] getImg1() {
@@ -247,20 +264,92 @@ public class ClientMessage implements Serializable {
 		this.mImg4 = mImg4;
 	}
 
-	public String[] getPhoneList() {
-		return mPhoneList;
+	public String getPhn() {
+		return mPhn;
 	}
 
-	public void setPhoneList(String[] mPhoneList) {
-		this.mPhoneList = mPhoneList;
+	public void setPhn(String mPhn) {
+		this.mPhn = mPhn;
 	}
 
-	public int[] getDeatilMsgid() {
-		return mDeatilMsgid;
+	public String getResultCode() {
+		return mResultCode;
 	}
 
-	public void setDeatilMsgid(int[] mDeatilMsgid) {
-		this.mDeatilMsgid = mDeatilMsgid;
+	public void setResultCode(String mResultCode) {
+		this.mResultCode = mResultCode;
+	}
+
+	public String getResultDate() {
+		return mResultDate;
+	}
+
+	public void setResultDate(String mResultDate) {
+		this.mResultDate = mResultDate;
+	}
+
+	public String getResultMsg() {
+		return mResultMsg;
+	}
+
+	public void setResultMsg(String mResultMsg) {
+		this.mResultMsg = mResultMsg;
+	}
+
+	public String getReg_dt() {
+		return mReg_dt;
+	}
+
+	public void setReg_dt(String mReg_dt) {
+		this.mReg_dt = mReg_dt;
+	}
+
+	public String getAdd1() {
+		return mAdd1;
+	}
+
+	public void setAdd1(String mAdd1) {
+		this.mAdd1 = mAdd1;
+	}
+
+	public String getAdd2() {
+		return mAdd2;
+	}
+
+	public void setAdd2(String mAdd2) {
+		this.mAdd2 = mAdd2;
+	}
+
+	public String getAdd3() {
+		return mAdd3;
+	}
+
+	public void setAdd3(String mAdd3) {
+		this.mAdd3 = mAdd3;
+	}
+
+	public String getAdd4() {
+		return mAdd4;
+	}
+
+	public void setAdd4(String mAdd4) {
+		this.mAdd4 = mAdd4;
+	}
+
+	public String getAdd5() {
+		return mAdd5;
+	}
+
+	public void setAdd5(String mAdd5) {
+		this.mAdd5 = mAdd5;
+	}
+
+	public StringBuffer getPhnList() {
+		return mPhnList;
+	}
+
+	public void setPhnList(StringBuffer mPhnList) {
+		this.mPhnList = mPhnList;
 	}
     
 }
